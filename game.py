4d5d77bd2f9f441724f0a32d5afe14b48b7ddb4f
CCT64 <chirachortkultorn@gmail.com>
2024-12-22 16:00:35 +0700

    Khun Danger

---
+++ game.py
@@ -0,0 +1,36 @@
+import pygame
+import time
+from paddle import*
+
+class Game():
+
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((800,600))
+        self.bg = pygame.image.load("images/bg.webp")
+        self.bg = pygame.transform.scale(self.bg, (800, 600))
+
+        self.paddle = pygame.image.load("images/paddle.jpg")
+        self.paddle = pygame.transform.scale(self.paddle, (50, 50))
+
+        self.player = Paddle(self.window, 350, 450, self.paddle)
+
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+
+        running = True
+        while (running):
+            delta_time = self.clock.tick(60) / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.window.blit(self.bg, (0,0))
+            self.player.draw()
+            self.player.move(delta_time)
+
+            pygame.display.update()
+
+game = Game()
+game.run()
